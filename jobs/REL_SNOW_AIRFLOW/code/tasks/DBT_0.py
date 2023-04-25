b83ec92edd5ed4c042409caf901e6a6cb9af2026
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-04-25 16:07:19 +0000

    ❌ Deleted     - Job        - REL_SNOW_Databricks
    🔁 Modified
        - Job
           - REL_SNOW_AIRFLOW

--- jobs/REL_SNOW_AIRFLOW/code/tasks/DBT_0.py
+++ jobs/REL_SNOW_AIRFLOW/code/tasks/DBT_0.py
@@ -2,13 +2,14 @@ def DBT_0():
     from datetime import timedelta
     from airflow.operators.bash import BashOperator
     envs = {}
+    envs["DBT_PROFILES_DIR"] = "/home/airflow/gcs/data"
     envs["DBT_SEND_ANONYMOUS_USAGE_STATS"] = "false"
     envs["DBT_FAIL_FAST"] = "true"
     envs["DBT_FULL_REFRESH"] = "true"
 
     return BashOperator(
         task_id = "DBT_0",
-        bash_command = "set -euxo pipefail; tmpDir=`mktemp -d`; git clone https://github.com/abhisheks-prophecy/test_repo_sql --branch main --single-branch $tmpDir; cd $tmpDir/test/SQL/uitesting/SQL_SnowflakeSharedBasic; dbt run; dbt test; ",
+        bash_command = "set -euxo pipefail; tmpDir=`mktemp -d`; git clone https://github.com/abhisheks-prophecy/sql_snowflake_public_child_1 --branch main --single-branch $tmpDir; cd $tmpDir/; dbt run --profile run_profile_snowflake; dbt test --profile run_profile_snowflake; ",
         env = envs,
         append_env = True,
         retry_exponential_backoff = True, 
