6ece71c555fc0c3d94d5264790f6f63118f8dbed
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-06-13 06:59:28 +0000

    ❇️ New     - Model        - env_uitesting_shared_excluded_model     - Job        - REL_SNOW_ProphecyManaged_Airflow
    🔁 Modified
        - Job
           - REL_SNOW_AIRFLOW

---
+++ jobs/REL_SNOW_ProphecyManaged_Airflow/code/dag.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import pendulum
+from datetime import timedelta
+import airflow
+from airflow import DAG
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+from tasks import DBT_0
+PROPHECY_RELEASE_TAG = "__PROJECT_ID_PLACEHOLDER__/__PROJECT_RELEASE_VERSION_PLACEHOLDER__"
+
+with DAG(
+    dag_id = "ovapyfz7oI7ZGqokO1KblQ_", 
+    schedule_interval = "0 0 19 * *", 
+    default_args = {"owner" : "Prophecy", "ignore_first_depends_on_past" : True, "pool" : "tB-MN8Ps", "do_xcom_push" : True}, 
+    start_date = pendulum.today('UTC'), 
+    end_date = pendulum.datetime(2023, 7, 3, tz = "UTC"), 
+    catchup = True
+) as dag:
+    DBT_0_op = DBT_0()
---
+++ jobs/REL_SNOW_ProphecyManaged_Airflow/code/tasks/DBT_0.py
@@ -0,0 +1,22 @@
+def DBT_0():
+    settings = {}
+    from datetime import timedelta
+    from airflow.operators.bash import BashOperator
+    envs = {}
+    envs["DBT_WARN_ERROR"] = "true"
+    envs["DBT_SEND_ANONYMOUS_USAGE_STATS"] = "false"
+    envs["DBT_FULL_REFRESH"] = "true"
+    envs["DBT_PROFILE_SECRET"] = "hwcTnR9DmvB6YVPYFtkxa"
+    envs["GIT_TOKEN_SECRET"] = ""
+    envs["GIT_ENTITY"] = "branch"
+    envs["GIT_ENTITY_VALUE"] = "main"
+    envs["GIT_SSH_URL"] = "https://github.com/abhisheks-prophecy/sql_snowflake_public_child_1"
+    envs["GIT_SUB_PATH"] = ""
+
+    return BashOperator(
+        task_id = "DBT_0",
+        bash_command = f"$PROPHECY_HOME/run_dbt.sh \"dbt deps --profile run_profile; dbt seed --profile run_profile --exclude env_uitesting_shared_excluded_model; dbt run --profile run_profile --exclude env_uitesting_shared_excluded_model; dbt test --profile run_profile --exclude env_uitesting_shared_excluded_model; \"",
+        env = envs,
+        append_env = True,
+        **settings
+    )
