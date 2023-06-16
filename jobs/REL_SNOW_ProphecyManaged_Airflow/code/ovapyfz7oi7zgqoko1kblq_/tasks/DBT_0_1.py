9556674ed95ef48d5fbe435b03e3ff3b69ecd44b
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-06-16 11:44:56 +0000

    🔁 Modified     - Job        - REL_SNOW_AIRFLOW        - REL_SNOW_ProphecyManaged_Airflow

--- jobs/REL_SNOW_ProphecyManaged_Airflow/code/ovapyfz7oi7zgqoko1kblq_/tasks/DBT_0_1.py
+++ jobs/REL_SNOW_ProphecyManaged_Airflow/code/ovapyfz7oi7zgqoko1kblq_/tasks/DBT_0_1.py
@@ -7,14 +7,10 @@ def DBT_0_1():
     envs["DBT_FAIL_FAST"] = "true"
     envs["DBT_LOG_PATH"] = "logssnow.txt"
     envs["DBT_FULL_REFRESH"] = "true"
-
-    if "Cautious":
-        envs["DBT_INDIRECT_SELECTION"] = "Cautious"
-
     envs["DBT_PROFILE_SECRET"] = "hwcTnR9DmvB6YVPYFtkxa"
     envs["GIT_TOKEN_SECRET"] = ""
     envs["GIT_ENTITY"] = "branch"
-    envs["GIT_ENTITY_VALUE"] = "main"
+    envs["GIT_ENTITY_VALUE"] = "dev"
     envs["GIT_SSH_URL"] = "https://github.com/abhisheks-prophecy/sql_snowflake_public_child_1"
     envs["GIT_SUB_PATH"] = ""
 
